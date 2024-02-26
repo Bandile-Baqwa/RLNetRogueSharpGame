5a924b865886e266590605b56ff54c037b1bc3d1
Bandile Shaun Baqwa <bsbaqwa@gmail.com>
2024-02-26 10:50:32 +0200

    Player.Cs is created and inherts Actor

---
+++ Core/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLNETConsoleGame.Core
+{
+    public class Player : Actor
+    {
+        public Player()                 // of these properties are aval because it inherited from class Actor
+        {
+            Awareness = 15;
+            Name = "Sterling Archer";
+            Color = Colors.Player;
+            Symbol = '@';
+            X = 10;                     //X and Y are the starting pos of player
+            Y = 10;
+        }
+    }
+}
